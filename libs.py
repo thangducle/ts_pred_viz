c725ca3880f4b1193daba72101b03cd720b13313
Thang Le <leducthang290193@gmail.com>
2021-05-15 15:13:56 +0900

    update model

--- libs.py
+++ libs.py
@@ -96,7 +96,9 @@ class WindowGenerator():
             plt.scatter(self.label_indices, labels[n, :, label_col_index], edgecolors='k', label='Labels', c='#2ca02c', s=64)
             if model is not None:
                 predictions = model(inputs)
-                plt.scatter(self.label_indices, predictions[n, :, label_col_index], marker='X', edgecolors='k', label='Predictions', c='#ff7f0e', s=64)
+                print(predictions.shape)
+                print(len(self.label_indices))
+                plt.scatter(self.label_indices, predictions[n, :], marker='X', edgecolors='k', label='Predictions', c='#ff7f0e', s=64)
 
             if n == 0:
                 plt.legend()
